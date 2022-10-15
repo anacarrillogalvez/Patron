905320310c3545f1534fdd746c217a70592d9ab1
anacarrillogalvez <anacarrillo49@aragon.unam.mx>
2022-10-14 19:02:27 -0500

    se agrega

--- Patron.cpp
+++ Patron.cpp
@@ -2,8 +2,9 @@
 #include <conio.h>
 #include<string.h>
 
-#define maxTexto 500
-#define maxPatron 100
+
+#define maxCadena 500
+#define maxPatronabuscar 100
 
 using namespace std;
 
@@ -11,42 +12,43 @@ void FuerzaBruta(char[], char[], int, int);
 
 int main(void)
 {
-    char Texto[maxTexto];
-    char Patron[maxPatron];
-    int n, m;
+    char Cadena[maxCadena];
+    char Patron[maxPatronabuscar];
+    int n;
+    int m;
 
     cout << "Ingrese la cadena:";
-    cin>>(Texto);
-    n = strlen(Texto);
+    cin>>(Cadena);
+    n = strlen(Cadena);
     do {
         cout << "\nIngrese el patron a buscar:";
         cin >> Patron;
         m = strlen(Patron);
       
     } while (m > n); 
+
     
-    FuerzaBruta(Texto, Patron, n, m);   
+    FuerzaBruta(Cadena, Patron, n, m);   
+
 }
 
 
-void FuerzaBruta(char texto[], char patron[], int n, int m)
+void FuerzaBruta(char cadena[], char patron[], int n, int m)
 {
     int i, j, k, cont = 0;
     char temp[100];
-
-        for (i = 0; i <= n; i++)
+    for (i = 0; i <= n; i++)
     {
-            for (j = i, k = 0; j < m; j++, k++)
-
-            temp[k] = texto[i + k];
-            temp[k] = '\0';
-            if (strcmp(patron, temp) == 0)
+        for (j = i, k = 0; j < m; j++, k++)
+            temp[k] = cadena[i + k];
+        temp[k] = '\0';
+        if (strcmp(patron, temp) == 0)
         {
-            cout << "Posicion: " << i << "\n";
+            cout << "\nPosicion: " << i;
             cont++;
         }
         m++;
     }
-    cout << "\nNo. de repeticiones:" << cont << endl;
+    cout << "\nNo de repeticiones:" << cont << endl;
 
 }
